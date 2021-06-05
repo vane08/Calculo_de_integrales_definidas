efea17d12f7271ba8d8a123f83d47133e4be7a6f
vane08 <82453121+vane08@users.noreply.github.com>
2021-06-04 22:40:16 -0500

    CalculoDeIntegralesDefinidas1VanessaCruzHuitron3202.m

--- CalculoDeIntegralesDefinidas1VanessaCruzHuitron3202.m
+++ CalculoDeIntegralesDefinidas1VanessaCruzHuitron3202.m
@@ -1,5 +1,29 @@
 %Titulo: Calculo de integrales definidas(1)
-%Nombre:Vanessa Cruz Huitrón 
+%Nombre:Vanessa Cruz HuitrÃ³n 
 %Grupo:3202
 %Fecha: jueves 3 de Mayo del 2021
 
+clc, clear  
+pkg load symbolic
+syms t;
+int(t.^2)
+ans = (sym)
+disp ("Calculo de integrales definidas 1");
+intervalo_a=1
+intervalo_b=4
+disp ("(t.^2 )");
+disp ("Sustitucion:");
+R=(2*intervalo_b.^3/2./(3/2))-(2*intervalo_a.^3/2./(3/2));
+disp ("Formula:");
+disp ("d=velocidad* tiempo");    
+disp("Velocidad")
+V=R
+disp("Tiempo")
+t=2
+t1=3
+t2=4            
+disp("Sustitucion de valores:")
+disp ("d= v*t");
+distancia_particula=R+R*2+R*3+R*4; 
+disp ("Distancia recorrida por la particula");
+distancia_recorrida_por_la_particula=distancia_particula
